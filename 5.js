ee31a8a27c00e2c950303dac3ed983cb70ab3e1d
rayyanansari17 <rayyanshajiansari@gmail.com>
2025-10-28 10:55:27 +0000

    created files of functions in js

---
+++ 5.js
@@ -0,0 +1,29 @@
+// // 5. arrow functions
+
+function world(){
+    console.log("world");
+}
+hello()
+
+
+
+// function with paramemtet
+
+let add = function (num1, num2){
+    return num1 + num2;
+}
+
+let total = add(10,5)
+console.log(total);
+
+
+
+// arrow function, this function update was in ES6
+
+let add = (num, num3) =>{
+    return num + num3
+}
+
+let total1 = add1(10,20);
+console.log(total1);
+
